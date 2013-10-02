61696abddd0fadaa76253d17bb4779a2ba60a00f
Nico G <mail@ng-websolutions.de>
2013-10-02 19:03:52 +0200

    Bugfix issue #4

--- functions/functions.ep.inc.php
+++ functions/functions.ep.inc.php
@@ -30,17 +30,17 @@ $.fn.respimg = function() {
 	var ww = $(window).width();
 	img = $(this)
 	$(bp).each(function(key, val) {
-		if(ww>val) {
+		if(ww > val) {
 			MinSize=val
-			if(MinSize!=lastMinSize) {
-		    	$(this).each(function() {
-		    		if(img.attr("data-min"+MinSize)) {
-		    			img.attr("src", img.attr("data-min"+MinSize))
+			console.log(MinSize);
+		    	img.each(function() {
+		    		MinSizeSrc = $(this).attr("data-min"+MinSize);
+		    		dataActive = $(this).attr("data-active");
+		    		if(MinSizeSrc && (dataActive>ww || dataActive < MinSize)) {
+		    			$(this).attr("src", MinSizeSrc);
+		    			$(this).attr("data-active", MinSize);
 		    		}	    		
 		    	});
-				lastMinSize = MinSize	    				
-			}
-			return false;
 		}
 	})  
 };
@@ -52,7 +52,6 @@ $(document).ready(function() {
 		})}
 	, 500);
 });';
-
 		$jsfile = $REX['HTDOCS_PATH'].'files/addons/responsiveimg/js.js';
 		file_put_contents($jsfile, $jscode);		
 	}
@@ -71,11 +70,11 @@ function ep_replace_responsiveimg($params) {
 	    }
 	}
 	foreach ($tmp_array as $m1 => $breakpoints) {
-		$alternate_src = 'class="responsiveimg" src="index.php?rex_img_type='.$m1.'&rex_img_file=\1" data-min1="index.php?rex_img_type='.$m1.'&rex_img_file=\1" ';
+		$alternate_src = 'class="responsiveimg" src="index.php?rex_img_type='.$m1.'&rex_img_file=\1" data-min1="index.php?rex_img_type='.$m1.'&rex_img_file=\1" data-active="9999"';
 		foreach ($breakpoints as $minwidth => $responsive) {
 			$alternate_src .= 'data-min'.$minwidth.'="index.php?rex_img_type='.$responsive.'&rex_img_file=\1" ';
 		}
-		$out = preg_replace('#src="index.php\?rex_img_type='.$m1.'&rex_img_file=([a-zA-Z0-9-_\.]*)"#', $alternate_src, $out);
+		$out = preg_replace('#src="index.php\?rex_img_type='.$m1.'&amp;rex_img_file=(.*?)"#', $alternate_src, $out);
 	}
 	return $out;
 }
