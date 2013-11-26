09cd00134d6a5376deef49c265b961356b4447cc
Nico G <mail@ng-websolutions.de>
2013-11-26 19:06:00 +0100

    V 1.0.1

--- config.inc.php
+++ config.inc.php
@@ -6,7 +6,7 @@ $REX['ADDON']['page'][$mypage] = $mypage; # zum Erzeugen von Backend-Urls
 $REX['ADDON']['name'][$mypage] = 'ResponsiveImg'; # name fuer Anzeige
 $REX['ADDON']['perm'][$mypage] = 'responsiveimg[]'; # fuer Benutzerrechte
 $REX['PERM'][] = 'responsiveimg[]';
-$REX['ADDON']['version'][$mypage] = '1.0';
+$REX['ADDON']['version'][$mypage] = '1.0.1';
 $REX['ADDON']['author'][$mypage] = 'NGW';
 
 require_once ($REX['INCLUDE_PATH'].'/addons/responsiveimg/functions/functions.ep.inc.php');
